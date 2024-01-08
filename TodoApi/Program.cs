02e69d1fc66d5dca2345f6d05587608ee1be60dc
rishiprakash <rishiprakash99@gmail.com>
2024-01-08 21:50:26 +0000

    Added DbContext and APIs for /get and /add of a todo

---
+++ TodoApi/Program.cs
@@ -0,0 +1,30 @@
+﻿using TodoApi;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Adding database context to DI container
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/get",async (TodoDb db) => { //Here also the lambda handler can get the param populated by DI Container
+    var todos = await db.Todos.ToListAsync();
+    return todos;
+});
+app.MapPost("/add", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+    //Here Results is a factory for IResult which is
+    // an interface that represents the result of an HTTP operation
+    return Results.Created($"/get/{todo.Id}", todo);
+   
+});
+
+app.Run();
+
